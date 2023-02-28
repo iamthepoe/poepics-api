f46c7e4099cfce74f0f680da6c9734725dbd6036
iamthepoe <lucagamerbrime@gmail.com>
2023-02-28 10:56:50 -0300

    updating imports

--- src/models/ImageModel.js
+++ src/models/ImageModel.js
@@ -1,4 +1,4 @@
-const ImageSchema = require('../schemas/Images.js');
+const ImageSchema = require('../schemas/Image.js');
 const mongoose = require('mongoose');
 const ImageModel = mongoose.model('Image', ImageSchema);
 module.exports = ImageModel;
